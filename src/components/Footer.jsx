82a5922a4d966f98de35e45cc18e19d58adcc84f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-28 12:41:11 -0300

    feat: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Link } from '@/components/ui/Link.jsx';
+import { SocialIcon } from '@/components/ui/SocialIcon.jsx';
+
+export function Footer() {
+  return (
+    <footer>
+      <p>
+        © Stebeus 2026.{' '}
+        <Link to="http://opensource.org/license/mit">MIT License</Link>
+      </p>
+      <Link to="http://github.com/stebeus">
+        <SocialIcon brand="github" alt="GitHub profile" />
+      </Link>
+    </footer>
+  );
+}

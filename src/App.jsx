a63980ed5a33d54c0d1c92122298c4e3c98d3912
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 14:51:42 -0300

    feat: add score state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Footer } from './components/Footer.jsx';
 import { Grid } from './components/Grid.jsx';
 import { Header } from './components/Header.jsx';
@@ -5,9 +7,11 @@ import { Header } from './components/Header.jsx';
 import './assets/App.css';
 
 export function App() {
+  const [score, setScore] = useState(0);
+
   return (
     <>
-      <Header />
+      <Header score={score} />
       <main>
         <p>Tap a card to score, but never click the same Pokémon twice!</p>
         <Grid />

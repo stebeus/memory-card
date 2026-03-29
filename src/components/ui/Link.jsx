d254b7f5432893d0969f4e496b0101ce2a988340
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 11:06:14 -0300

    fix: correct url scheme regex

--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -1,8 +1,8 @@
 import { BASE_URL } from '@/constants.js';
 
 export function Link({ to = BASE_URL, children }) {
-  const urlScheme = /http|https/;
-  const isExternal = urlScheme.test(`${to}://`);
+  const urlScheme = /http:[//]|https:[//]/;
+  const isExternal = urlScheme.test(to);
 
   const isTargetBlank = isExternal && {
     target: '_blank',

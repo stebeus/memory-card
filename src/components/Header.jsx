34059c18e01d769c1ecb1127f1b9de63694d8bb2
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 14:33:27 -0300

    feat: render Scoreboard in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from './ui/Link.jsx';
+import { Scoreboard } from './ui/Scoreboard.jsx';
 
-export function Header() {
+export function Header({ score, bestScore }) {
   return (
     <header>
       <h1 className="logo">
         <Link>Memory card</Link>
       </h1>
+      <Scoreboard score={score} bestScore={bestScore} />
     </header>
   );
 }

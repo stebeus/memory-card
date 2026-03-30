e482f378a60b9e296ce488861ef4cc1f4972c449
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-30 17:25:23 -0300

    chore: remove vite config documentation url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/memory-card/',
   resolve: {
